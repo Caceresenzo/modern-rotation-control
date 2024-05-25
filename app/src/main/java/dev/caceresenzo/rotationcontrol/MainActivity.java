5a51534b8b900625929804b6cf9532b1c84156a5
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-25 20:00:23 +0200

    feat(main): ask for permissions

--- app/src/main/java/dev/caceresenzo/rotationcontrol/MainActivity.java
+++ app/src/main/java/dev/caceresenzo/rotationcontrol/MainActivity.java
@@ -1,12 +1,26 @@
 package dev.caceresenzo.rotationcontrol;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private final ActivityResultLauncher<Intent> permissionActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            (result) -> checkPermissions(true)
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,38 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(false);
         }
+
+        checkPermissions(true);
+    }
+
+    public boolean checkPermissions(boolean request) {
+        Uri uri = Uri.parse("package:" + getPackageName());
+
+        if (!Settings.System.canWrite(this)) {
+            if (request) {
+                Log.i(TAG, "Cannot yet write settings");
+                Toast.makeText(this, R.string.require_settings_write_permission, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, uri);
+                permissionActivityResultLauncher.launch(intent);
+            }
+
+            return false;
+        }
+
+        if (!Settings.canDrawOverlays(this)) {
+            if (request) {
+                Log.i(TAG, "Cannot yet draw overlays");
+                Toast.makeText(this, R.string.require_overlay_permission, Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri);
+                permissionActivityResultLauncher.launch(intent);
+            }
+
+            return false;
+        }
+
+        return true;
     }
 
 }
