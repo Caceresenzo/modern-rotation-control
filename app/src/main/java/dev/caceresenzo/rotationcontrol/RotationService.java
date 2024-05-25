2e491a3499c03f4309895c5d539e1b36abac857b
Caceresenzo <caceresenzo1502@gmail.com>
2024-05-25 23:08:53 +0200

    feat(service): resend notification after delete

--- app/src/main/java/dev/caceresenzo/rotationcontrol/RotationService.java
+++ app/src/main/java/dev/caceresenzo/rotationcontrol/RotationService.java
@@ -1,10 +1,13 @@
 package dev.caceresenzo.rotationcontrol;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
@@ -19,11 +22,11 @@ public class RotationService extends Service {
     public static final String CHANNEL_ID = "Controls";
     public static final int NOTIFICATION_ID = 1;
 
-    public static final String INTENT_ACTION = "ACTION";
-    public static final int ACTION_START = 1;
+    public static final String ACTION_START = "START";
+    public static final String ACTION_REFRESH_NOTIFICATION = "REFRESH";
 
     private final IBinder binder = new LocalBinder();
-    private NotificationCompat.Builder notificationBuilder;
+    private Notification.Builder notificationBuilder;
 
     @Nullable
     @Override
@@ -37,19 +40,49 @@ public class RotationService extends Service {
 
         createNotificationChannel();
 
-        notificationBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID);
+        notificationBuilder = new Notification.Builder(getApplicationContext(), CHANNEL_ID);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopForeground(STOP_FOREGROUND_REMOVE);
+        stopSelf();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i(TAG, "OnStartCommand");
+        Log.i(TAG, String.format("OnStartCommand flags=%d startId=%d", flags, startId));
+
+        String action = intent.getAction();
+        if (action == null) {
+            Log.i(TAG, "null action");
+
+            return START_NOT_STICKY;
+        }
 
         notificationBuilder
-                .setContentTitle("Hello World");
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setOngoing(true)
+                .setContentTitle("Hello World")
+                .setDeleteIntent(newRefreshPendingIntent());
 
-        startForeground(NOTIFICATION_ID, notificationBuilder.build());
+        switch (action) {
+            case ACTION_START: {
+                startForeground(NOTIFICATION_ID, notificationBuilder.build(), ServiceInfo.FOREGROUND_SERVICE_TYPE_SPECIAL_USE);
+                return START_STICKY;
+            }
 
-        return super.onStartCommand(intent, flags, startId);
+            case ACTION_REFRESH_NOTIFICATION: {
+                getNotificationManager().notify(NOTIFICATION_ID, notificationBuilder.build());
+                return START_STICKY;
+            }
+
+            default: {
+                Log.i(TAG, String.format("unknown action - action=%s", action));
+
+                return START_NOT_STICKY;
+            }
+        }
     }
 
     private void createNotificationChannel() {
@@ -60,15 +93,28 @@ public class RotationService extends Service {
         notificationChannel.enableLights(false);
         notificationChannel.setLockscreenVisibility(NotificationCompat.VISIBILITY_SECRET);
 
-        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
-        notificationManager.createNotificationChannel(notificationChannel);
+        getNotificationManager().createNotificationChannel(notificationChannel);
     }
 
-    static class LocalBinder extends Binder {}
+    private PendingIntent newRefreshPendingIntent() {
+        Intent intent = new Intent(getApplicationContext(), RotationService.class);
+        intent.setAction(ACTION_REFRESH_NOTIFICATION);
+
+        return PendingIntent.getService(
+                this,
+                2,
+                intent,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+        );
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+    }
 
     public static void start(Context context) {
         Intent intent = new Intent(context.getApplicationContext(), RotationService.class);
-        intent.putExtra(RotationService.INTENT_ACTION, RotationService.ACTION_START);
+        intent.setAction(ACTION_START);
 
         context.startForegroundService(intent);
     }
@@ -77,6 +123,9 @@ public class RotationService extends Service {
         Intent intent = new Intent(context, RotationService.class);
 
         context.stopService(intent);
+    }
+
+    static class LocalBinder extends Binder {
     }
 
 }
