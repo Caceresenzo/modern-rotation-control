d72782a654d54298fd74f3ccd769181fdafb6615
Caceresenzo <caceresenzo1502@gmail.com>
2024-09-01 22:23:37 +0200

    release: bump to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.caceresenzo.rotationcontrol"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.1.0"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
