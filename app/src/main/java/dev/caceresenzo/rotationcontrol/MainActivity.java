9938e14e6eb6adcde9eabc452b620526ab6545a1
Caceresenzo <caceresenzo1502@gmail.com>
2026-03-01 21:44:32 +0100

    feat(github): add link to project

--- app/src/main/java/dev/caceresenzo/rotationcontrol/MainActivity.java
+++ app/src/main/java/dev/caceresenzo/rotationcontrol/MainActivity.java
@@ -1,19 +1,25 @@
 package dev.caceresenzo.rotationcontrol;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.preference.PreferenceManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String GITHUB_URL = "https://github.com/Caceresenzo/modern-rotation-control";
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -48,6 +54,38 @@ public class MainActivity extends AppCompatActivity {
                 RotationService.start(this);
             }
         }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            getOnBackPressedDispatcher().onBackPressed();
+            return true;
+        }
+
+        if (id == R.id.action_github) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(GITHUB_URL));
+
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException exception) {
+                Toast.makeText(this, R.string.no_browser_found, Toast.LENGTH_SHORT).show();
+            }
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public boolean checkPermissions(boolean request) {
